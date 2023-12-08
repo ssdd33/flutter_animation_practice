3b70272d4bf299e7d9089eeaa0c511c99f1669d3
ssdd33 <beeblu.0@gmail.com>
2023-12-08 12:08:44 +0900

    animationWithoutTriggeringEvent

--- lib/main.dart
+++ lib/main.dart
@@ -20,17 +20,36 @@ class AnimationPage extends StatefulWidget {
   State<AnimationPage> createState() => _AnimationPageState();
 }
 
-class _AnimationPageState extends State<AnimationPage> {
+class _AnimationPageState extends State<AnimationPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+
+  @override
+  void initState() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    animationController.repeat(reverse: true);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: GestureDetector(
-              onTap: () {
-                setState(() {});
-              },
-              child: const Text('Hello World!')),
+          child: AnimatedIcon(
+            icon: AnimatedIcons.play_pause,
+            progress: animationController,
+            size: 200,
+          ),
         ),
       ),
     );

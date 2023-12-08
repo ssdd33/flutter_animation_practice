76cf357c3b7879dd4bd503e69f50195d3ae0dfd7
ssdd33 <beeblu.0@gmail.com>
2023-12-08 13:11:41 +0900

    animatedBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -13,26 +13,59 @@ class MainApp extends StatelessWidget {
   }
 }
 
-class AnimationPage extends StatefulWidget {
+class AnimationPage extends StatelessWidget {
   const AnimationPage({super.key});
 
   @override
-  State<AnimationPage> createState() => _AnimationPageState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(child: MyAnimatedWidget()),
+      ),
+    );
+  }
 }
 
-class _AnimationPageState extends State<AnimationPage> {
+class MyAnimatedWidget extends StatefulWidget {
+  const MyAnimatedWidget({super.key});
+
+  @override
+  State<MyAnimatedWidget> createState() => _MyAnimatedWidgetState();
+}
+
+class _MyAnimatedWidgetState extends State<MyAnimatedWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(duration: const Duration(seconds: 1), vsync: this);
+    animation = Tween<double>(begin: 0, end: 1).animate(animationController);
+    animationController.repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: GestureDetector(
-              onTap: () {
-                setState(() {});
-              },
-              child: const Text('Hello World!')),
-        ),
-      ),
+    return AnimatedBuilder(
+      animation: animation,
+      builder: (context, child) {
+        return Opacity(
+            opacity: animation.value,
+            child: Container(
+              height: 200,
+              width: 200,
+              color: Colors.blue,
+            ));
+      },
     );
   }
 }

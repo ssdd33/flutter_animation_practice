abe61049468c45c48f82c1cf2d1173ba7e0e61d8
ssdd33 <beeblu.0@gmail.com>
2023-12-08 11:09:53 +0900

    animatedContainer

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,47 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return const AnimationPage();
+  }
+}
+
+class AnimationPage extends StatefulWidget {
+  const AnimationPage({super.key});
+
+  @override
+  State<AnimationPage> createState() => _AnimationPageState();
+}
+
+class _AnimationPageState extends State<AnimationPage> {
+  bool isExpanded = false;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                isExpanded = !isExpanded;
+              });
+            },
+            child: AnimatedContainer(
+              duration: const Duration(seconds: 1),
+              height: isExpanded ? 200 : 120,
+              width: isExpanded ? 200 : 120,
+              color: isExpanded ? Colors.blue : Colors.red,
+              curve: Curves.easeInExpo,
+              child: const Center(
+                child: Text(
+                  'click me!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );

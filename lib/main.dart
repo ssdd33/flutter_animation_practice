ec7087f7c1c1c77be0db12fc8b596c4ba937421d
ssdd33 <beeblu.0@gmail.com>
2023-12-08 13:00:34 +0900

    animatedSwitcher

--- lib/main.dart
+++ lib/main.dart
@@ -13,26 +13,66 @@ class MainApp extends StatelessWidget {
   }
 }
 
-class AnimationPage extends StatefulWidget {
+class AnimationPage extends StatelessWidget {
   const AnimationPage({super.key});
 
   @override
-  State<AnimationPage> createState() => _AnimationPageState();
-}
-
-class _AnimationPageState extends State<AnimationPage> {
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: GestureDetector(
-              onTap: () {
-                setState(() {});
-              },
-              child: const Text('Hello World!')),
+          child: AnimatedSwitcherWidget(),
         ),
       ),
+    );
+  }
+}
+
+class AnimatedSwitcherWidget extends StatefulWidget {
+  const AnimatedSwitcherWidget({super.key});
+
+  @override
+  State<AnimatedSwitcherWidget> createState() => _AnimatedSwitcherWidgetState();
+}
+
+class _AnimatedSwitcherWidgetState extends State<AnimatedSwitcherWidget> {
+  bool _isOn = false;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        AnimatedSwitcher(
+          duration: const Duration(seconds: 1),
+          transitionBuilder: (Widget child, Animation<double> animation) {
+            return ScaleTransition(scale: animation, child: child);
+          },
+          child: _isOn
+              ? Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                  size: 200,
+                  key: UniqueKey(),
+                )
+              : Icon(
+                  Icons.favorite_border,
+                  color: Colors.grey,
+                  size: 200,
+                  key: UniqueKey(),
+                ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              _isOn = !_isOn;
+            });
+          },
+          child: Text(_isOn ? 'off' : 'on'),
+        )
+      ],
     );
   }
 }

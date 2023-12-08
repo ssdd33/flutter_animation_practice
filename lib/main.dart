e66c160d3dbbddffccf48ff82a7e2963788fb424
ssdd33 <beeblu.0@gmail.com>
2023-12-08 11:52:04 +0900

    animatedPositioned

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -21,16 +23,47 @@ class AnimationPage extends StatefulWidget {
 }
 
 class _AnimationPageState extends State<AnimationPage> {
+  final bool _isTop = true;
+  double _top = 5;
+  double _left = 100;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: GestureDetector(
-              onTap: () {
-                setState(() {});
-              },
-              child: const Text('Hello World!')),
+          child: Container(
+            height: 300,
+            width: 300,
+            color: Colors.blue,
+            child: Stack(
+              children: [
+                AnimatedPositioned(
+                    duration: const Duration(milliseconds: 500),
+                    top: _top,
+                    left: _left,
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _top = Random().nextDouble() * 255;
+                          _left = Random().nextDouble() * 255;
+                          if (_top < 5) {
+                            _top = 5;
+                          }
+                          if (_left < 5) {
+                            _left = 5;
+                          }
+                        });
+                      },
+                      child: const Icon(
+                        Icons.bug_report,
+                        color: Colors.white,
+                        size: 50,
+                      ),
+                    ))
+              ],
+            ),
+          ),
         ),
       ),
     );

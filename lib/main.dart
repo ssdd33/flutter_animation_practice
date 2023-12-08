7ac428ce336c37476e89ef7e5f303f1dbea3b758
ssdd33 <beeblu.0@gmail.com>
2023-12-08 15:36:14 +0900

    easeIn/easeOut tweening

--- lib/main.dart
+++ lib/main.dart
@@ -13,26 +13,57 @@ class MainApp extends StatelessWidget {
   }
 }
 
-class AnimationPage extends StatefulWidget {
+class AnimationPage extends StatelessWidget {
   const AnimationPage({super.key});
 
   @override
-  State<AnimationPage> createState() => _AnimationPageState();
-}
-
-class _AnimationPageState extends State<AnimationPage> {
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: GestureDetector(
-              onTap: () {
-                setState(() {});
-              },
-              child: const Text('Hello World!')),
+          child: CurvedAnmationWidget(),
         ),
       ),
     );
+  }
+}
+
+class CurvedAnmationWidget extends StatefulWidget {
+  const CurvedAnmationWidget({super.key});
+
+  @override
+  State<CurvedAnmationWidget> createState() => _CurvedAnmationWidgetState();
+}
+
+class _CurvedAnmationWidgetState extends State<CurvedAnmationWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    _controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 5));
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
+    super.initState();
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaleTransition(
+        scale: _animation,
+        child: Container(width: 200, height: 100, color: Colors.blue));
+
+    return RotationTransition(
+        turns: _animation,
+        child: Container(height: 200, width: 200, color: Colors.blue));
   }
 }

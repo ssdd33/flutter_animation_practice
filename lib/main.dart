da70ca06357d8d2bd9effc39df567b7520f32e92
ssdd33 <beeblu.0@gmail.com>
2023-12-08 11:18:19 +0900

    animatedOpacity

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,42 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return const AnimationPage();
+  }
+}
+
+class AnimationPage extends StatefulWidget {
+  const AnimationPage({super.key});
+
+  @override
+  State<AnimationPage> createState() => _AnimationPageState();
+}
+
+class _AnimationPageState extends State<AnimationPage> {
+  bool _isVisible = false;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                _isVisible = !_isVisible;
+              });
+            },
+            child: AnimatedOpacity(
+              opacity: _isVisible ? 1.0 : 0.1,
+              duration: const Duration(milliseconds: 500),
+              child: Container(
+                  height: 300,
+                  width: 300,
+                  color: Colors.blue,
+                  child: const Center(
+                      child: Text('click me!',
+                          style: TextStyle(color: Colors.black)))),
+            ),
+          ),
         ),
       ),
     );

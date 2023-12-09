9a14cee282d4b79b65d238a698083de51838adc0
ssdd33 <beeblu.0@gmail.com>
2023-12-09 13:52:25 +0900

    bouncing_ball

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/physics.dart';
 
 void main() {
   runApp(const MainApp());
@@ -20,17 +21,54 @@ class AnimationPage extends StatefulWidget {
   State<AnimationPage> createState() => _AnimationPageState();
 }
 
-class _AnimationPageState extends State<AnimationPage> {
+class _AnimationPageState extends State<AnimationPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late SpringSimulation _simulation;
+
+  @override
+  void initState() {
+    super.initState();
+    // bound -> 애니메이션 value의 min,max 설정
+    _controller = AnimationController(
+        vsync: this, lowerBound: 0, upperBound: double.infinity);
+
+    _simulation = SpringSimulation(
+      const SpringDescription(mass: 0.8, stiffness: 200, damping: 10),
+      0,
+      100,
+      0,
+    );
+
+    _controller.animateWith(_simulation);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: GestureDetector(
-              onTap: () {
-                setState(() {});
-              },
-              child: const Text('Hello World!')),
+          child: AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) {
+                return Transform.translate(
+                  offset: Offset(150, _controller.value),
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    decoration: const BoxDecoration(
+                      color: Colors.red,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                );
+              }),
         ),
       ),
     );

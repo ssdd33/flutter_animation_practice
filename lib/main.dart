d762128c21f60bcf446f40e9fc36926fea8958be
ssdd33 <beeblu.0@gmail.com>
2023-12-09 16:12:19 +0900

    drag_interaction

--- lib/main.dart
+++ lib/main.dart
@@ -20,19 +20,66 @@ class AnimationPage extends StatefulWidget {
   State<AnimationPage> createState() => _AnimationPageState();
 }
 
-class _AnimationPageState extends State<AnimationPage> {
+class _AnimationPageState extends State<AnimationPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Offset _startPosition;
+  late Offset _endPosition;
+  Offset _position = const Offset(0, 0);
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      upperBound: double.infinity,
+      lowerBound: double.infinity,
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _onPanStart(DragStartDetails details) {
+    _controller.stop();
+    _startPosition = details.globalPosition;
+  }
+
+  void _onPanUpdate(DragUpdateDetails details) {
+    setState(() {
+      _position += details.delta;
+    });
+  }
+
+  void _onPanEnd(DragEndDetails details) {}
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: GestureDetector(
-              onTap: () {
-                setState(() {});
-              },
-              child: const Text('Hello World!')),
-        ),
-      ),
+          body: GestureDetector(
+        onPanStart: _onPanStart,
+        onPanUpdate: _onPanUpdate,
+        onPanEnd: _onPanEnd,
+        child: Container(
+            alignment: Alignment.center,
+            color: Colors.white,
+            child: AnimatedBuilder(
+                animation: _controller,
+                builder: (context, child) {
+                  return Transform.translate(
+                      offset: Offset(_position.dx, _position.dy),
+                      child: Container(
+                          width: 100,
+                          height: 100,
+                          decoration: const BoxDecoration(
+                              color: Colors.redAccent,
+                              shape: BoxShape.circle)));
+                })),
+      )),
     );
   }
 }
